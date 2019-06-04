3b037bfd081a94c4b4e08336cb17984d5e9c8598
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-06-04 03:25:22 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price)
+    params.require(:product).permit(:name, :price, :inventory, :description)
   end
 end
